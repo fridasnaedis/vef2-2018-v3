1ed1f73d0c6de9ba9c1fb9fa9de49b74b4dba474
fridasnaedis <fridasnaedis@gmail.com>
2018-03-01 23:55:47 +0000

    heroku

--- api.js
+++ api.js
@@ -14,6 +14,34 @@ function catchErrors(fn) {
   return (req, res, next) => fn(req, res, next).catch(next);
 }
 
-/* todo útfæra api */
+router.post('/', async (req, res) => { 
+  const { title = '', text ='', datetime = '' } = req.body;
+  const response = await create({ title, text, datetime });
+  res.json(...response.rows);
+});
+
+router.get('/', async (req, res) => {
+  const response = await readAll();
+  res.json(response.rows);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const response = await readOne(id);
+  res.json(...response.rows);
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title = '', text = '', datetime = '' } = req.body;
+  const response = await update(id, {title, text, datetime });
+  res.json(...response.rows);
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  await del(id);
+  res.send();
+});
 
 module.exports = router;
--- notes.js
+++ notes.js
@@ -1,6 +1,22 @@
 /* todo sækja pakka sem vantar  */
+const { Client } = require('pg');
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL || 'postgres://:@localhost/v3';
+
+async function queryDB(query, values = []) {
+  const client = new Client({ connectionString });
+  await client.connect();
+  let result;
+  try {
+    result = await client.query(query, values);
+  } catch (err) {
+    console.log('Error');
+    throw err;
+  } finally {
+    await client.end();
+    return result;
+  }
+}
 
 /**
  * Create a note asynchronously.
@@ -13,6 +29,10 @@ const connectionString = process.env.DATABASE_URL;
  * @returns {Promise} Promise representing the object result of creating the note
  */
 async function create({ title, text, datetime } = {}) {
+  const query = 'INSERT INTO notes(datetime, title, text) VALUES($1, $2, $3) RETURNING *;';
+  const values = [datetime, title, text];
+  const result = await queryDB(query, values);
+  return result;
   /* todo útfæra */
 }
 
@@ -22,6 +42,9 @@ async function create({ title, text, datetime } = {}) {
  * @returns {Promise} Promise representing an array of all note objects
  */
 async function readAll() {
+  const query = 'SELECT * FROM notes;';
+  const result = await queryDB(query);
+  return result;
   /* todo útfæra */
 }
 
@@ -33,7 +56,10 @@ async function readAll() {
  * @returns {Promise} Promise representing the note object or null if not found
  */
 async function readOne(id) {
-  /* todo útfæra */
+  const query = 'SELECT * FROM notes WHERE id = $1;';
+  const values = [id];
+  const result = queryDB(query, values);
+  return result;
 }
 
 /**
@@ -48,7 +74,12 @@ async function readOne(id) {
  * @returns {Promise} Promise representing the object result of creating the note
  */
 async function update(id, { title, text, datetime } = {}) {
-  /* todo útfæra */
+  const query = 'UPDATE notes SET datetime = $2, title =  $3, text = $4 WHERE id = $1 RETURNING *;';
+  const values = [id, datetime, title, text];
+  console.log(values);
+  
+  const result = await queryDB(query, values);
+  return result;
 }
 
 /**
@@ -59,7 +90,10 @@ async function update(id, { title, text, datetime } = {}) {
  * @returns {Promise} Promise representing the boolean result of creating the note
  */
 async function del(id) {
-  /* todo útfæra */
+  const query = 'DELETE FROM notes WHERE id = $1;';
+  const values = [id];
+  const result = queryDB(query, values);
+  return result;
 }
 
 module.exports = {
